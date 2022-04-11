63a813f58b62e0ad4167bc61f990e6c72177eca5
Gabi <gabrielsotir18@yahoo.com>
2022-04-11 21:31:03 +0300

    finish exercice 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, numberOfTimes) {
+    let result = "";
+    if (numberOfTimes < 0) return "ERROR";
+    for(let i = 0 ; i < numberOfTimes ; i++){
+        result = result + text;
+    }
+    return result;
 };
 
 // Do not edit below this line
