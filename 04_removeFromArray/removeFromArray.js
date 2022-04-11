4759f057221cb44d65e7e43a3dff3538e30f1446
Gabi <gabrielsotir18@yahoo.com>
2022-04-11 22:11:30 +0300

    finish ex 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
 
+
+const removeFromArray = function(array, ...args) {
+    for(let i = 0 ; i < args.length ; i++){
+        valueToRemove = args[i];
+        array = array.filter(function(value){
+            return value !== valueToRemove;
+        });
+    }
+    return array;
 };
 
 // Do not edit below this line
