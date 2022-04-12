69f9fa313c4b0df61ac62e97b0879c48fbbb0f11
Gabi <gabrielsotir18@yahoo.com>
2022-04-12 13:20:31 +0300

    finish first 7 javascript exercices

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if( min < 0 || max < 0) return "ERROR";
+    if(typeof min != 'number' || typeof max != 'number') return "ERROR";
 
+    if (min > max){
+        let aux = min;
+        min = max;
+        max = aux;
+    }
+    bigMin=BigInt(min);
+    bigMax=BigInt(max);
+    let sum = 0n;
+    for(let i = bigMin ; i <= bigMax ; i++){
+        sum += i;
+    }
+    return Number(sum);
 };
 
 // Do not edit below this line
